16134bdaf38e227e1b82167ac6d821d9432bf6a4
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-04-15 14:19:42 -0400

    Day 21 Complete

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Anthony Bath on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
