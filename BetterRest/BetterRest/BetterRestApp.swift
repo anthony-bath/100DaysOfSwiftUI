97509d3b8e03e4920981507ef84caddbe113684b
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-04-21 07:18:31 -0400

    Day 27 Complete

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Anthony Bath on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
