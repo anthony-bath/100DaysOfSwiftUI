83f8b78121307a480009071286b7884859ae939b
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-04-25 07:48:09 -0400

    Day 33 Complete

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Anthony Bath on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
