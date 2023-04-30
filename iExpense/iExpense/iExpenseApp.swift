2aee45ec2f2840019a7386c83db9a8ec30ce7f9b
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-04-30 16:27:30 -0400

    Day 36 Complete

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Anthony Bath on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
