74dcf0368e9edc00c1dfe841c670de3e143da336
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-05-03 07:58:42 -0400

    Day 39 Complete

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Anthony Bath on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
