1d99e6c202c1f5b3d45ec4a82fba3269a9f1b58b
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-05-07 07:25:57 -0400

    Day 43 Complete

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Anthony Bath on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
