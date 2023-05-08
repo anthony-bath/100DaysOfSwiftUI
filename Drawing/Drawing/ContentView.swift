4b8899706cecf1df03674387b4519bd015f02df1
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-05-08 19:48:56 -0400

    Day 44 Complete

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -46,10 +46,43 @@ struct Arc: InsettableShape {
     }
 }
 
+struct Flower: Shape {
+    var petalOffset = -20.0
+    var petalWidth = 100.0
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
+            
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
+    }
+}
+
 struct ContentView: View {
+    @State private var petaloffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(.blue, lineWidth: 40)
+        Flower(petalOffset: petaloffset, petalWidth: petalWidth)
+            .fill(.red, style: FillStyle(eoFill: true))
+
+        Text("Offset")
+        Slider(value: $petaloffset, in: -40...40)
+            .padding([.horizontal, .bottom])
+
+        Text("Width")
+        Slider(value: $petalWidth, in: 0...100)
+            .padding(.horizontal)
     }
 }
 
