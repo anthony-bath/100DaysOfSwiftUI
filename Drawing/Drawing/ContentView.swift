5ed95c3f191fa5f9d58cb9ff405b4567d670498a
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-05-10 07:37:37 -0400

    Day 46 Complete

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -112,7 +112,7 @@ struct Checkboard : Shape {
                     let startX = colSize * Double(col)
                     let startY = rowSize * Double(row)
                     
-                    var rect = CGRect(x: startX, y: startY, width: colSize, height: rowSize)
+                    let rect = CGRect(x: startX, y: startY, width: colSize, height: rowSize)
                     path.addRect(rect)
                 }
             }
@@ -169,7 +169,7 @@ struct Spirograph : Shape {
     }
 }
 
-struct ContentView: View {
+struct SpirographyView: View {
     @State private var innerRadius = 125.0
     @State private var outerRadius = 75.0
     @State private var distance = 25.0
@@ -217,6 +217,156 @@ struct ContentView: View {
                 
             }
         }
+    }
+}
+
+struct Arrow : Shape {
+    var headHeight = 0.35
+    var tailWidth = 0.35
+    
+    var animatableData: AnimatablePair<Double, Double> {
+        get { AnimatablePair(headHeight, tailWidth) }
+        set {
+            headHeight = newValue.first
+            tailWidth = newValue.second
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let hHeight = rect.height * headHeight
+        let tWidth = rect.width * tailWidth
+        let headInset = (rect.width - tWidth) / 2
+        
+        path.move(to: CGPoint(x: rect.midX, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: hHeight))
+        path.addLine(to: CGPoint(x: headInset, y: hHeight))
+        path.addLine(to: CGPoint(x: headInset, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX - headInset, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX - headInset, y: hHeight))
+        path.addLine(to: CGPoint(x: rect.maxX, y: hHeight))
+        path.closeSubpath()
+        
+        return path
+    }
+}
+
+struct ArrowView: View {
+    @State private var headHeight = 0.35
+    @State private var tailWidth = 0.35
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Spacer()
+            
+            Arrow(headHeight: headHeight, tailWidth: tailWidth)
+                .fill(.blue)
+                .frame(width: 150, height: 300)
+                .onTapGesture {
+                    withAnimation {
+                        headHeight = Double.random(in: 0.35...0.7)
+                        tailWidth = Double.random(in: 0.35...0.7)
+                    }
+                }
+            
+            Spacer()
+            
+            
+            VStack(alignment: .leading) {
+                Text("Head Height: \(headHeight, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $headHeight, in: 0.35...0.7, step: 0.01)
+            }
+            .padding([.horizontal, .bottom])
+            
+            VStack(alignment: .leading) {
+                Text("Tail Width: \(tailWidth, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $tailWidth, in: 0.35...0.7, step: 0.01)
+            }
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
+    var gradientStartX = 0.0
+    var gradientStartY = 0.0
+    var gradientEndX = 1.0
+    var gradientEndY = 1.0
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: UnitPoint(x: gradientStartX, y: gradientStartY),
+                            endPoint: UnitPoint(x: gradientEndX, y: gradientEndY)
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct ContentView: View {
+    @State private var colorCycle = 0.0
+    @State private var gradientStartX = 0.0
+    @State private var gradientStartY = 0.0
+    @State private var gradientEndX = 1.0
+    @State private var gradientEndY = 1.0
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            ColorCyclingRectangle(
+                amount: colorCycle,
+                gradientStartX: gradientStartX,
+                gradientStartY: gradientStartY,
+                gradientEndX: gradientEndX,
+                gradientEndY: gradientEndY
+            )
+            .frame(width: 300, height: 300)
+            
+            Spacer()
+            
+            Group {
+                Text("Color Cycle: \(colorCycle, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $colorCycle)
+                
+                Text("Gradient Start X: \(gradientStartX, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $gradientStartX)
+                
+                Text("Gradient Start Y: \(gradientStartY, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $gradientStartY)
+                
+                Text("Gradient End X: \(gradientEndX, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $gradientEndX)
+                
+                Text("Gradient End Y: \(gradientEndY, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $gradientEndY)
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
