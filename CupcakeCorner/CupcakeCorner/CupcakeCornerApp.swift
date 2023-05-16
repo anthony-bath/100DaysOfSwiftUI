40f5837738d8101571f6935aa4f636353b6e8f1a
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-05-16 18:39:24 -0400

    Day 49 Complete

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Anthony Bath on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
