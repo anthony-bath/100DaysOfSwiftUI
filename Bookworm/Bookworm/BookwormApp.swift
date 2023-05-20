eb955847730994f7172ff7aba3ebeb2aa0fc900a
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-05-20 09:38:05 -0400

    Day 53 Complete

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Anthony Bath on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    @StateObject private var dataController = DataController()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
