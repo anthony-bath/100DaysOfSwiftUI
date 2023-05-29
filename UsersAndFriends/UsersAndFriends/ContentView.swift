4d4297fb07886b063370401b23f04d978e3f1790
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-05-29 10:40:37 -0400

    Day 60 Progress
    - download user data and display list

--- UsersAndFriends/UsersAndFriends/ContentView.swift
+++ UsersAndFriends/UsersAndFriends/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users: [User] = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(users, id: \.id) { user in
+                Text(user.name)
+            }
+            .padding(.bottom, 10)
+            .navigationTitle("Users")
         }
-        .padding()
+        .task {
+            if users.isEmpty {
+                await fetchData()
+            }
+        }
+    }
+    
+    func fetchData() async {
+        do {
+            let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")
+            let (data, response) = try await URLSession.shared.data(from: url!)
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                print("Non-200 Status Code")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+                let decodedResponse = try decoder.decode([User].self, from: data)
+                users = decodedResponse
+            } catch {
+                print("JSON decode failed: \(error.localizedDescription)")
+            }
+        } catch {
+            print("Download failed: \(error.localizedDescription)")
+        }
     }
 }
 
