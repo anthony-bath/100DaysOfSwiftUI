9a28a1b50e8fe3d230bf7e0b12148cda442522eb
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-06-04 14:12:22 -0400

    Day 62 Complete

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Anthony Bath on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
