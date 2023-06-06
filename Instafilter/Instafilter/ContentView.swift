7937dc058e4ca12207859db65784173d909a1e37
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-06-06 08:05:38 -0400

    Day 64 Complete

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -11,58 +11,8 @@ import SwiftUI
 import PhotosUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var showingImagePicker = false
-    @State var selectedItems: [PhotosPickerItem] = []
-    
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingImagePicker = true
-            }
-        }
-        .sheet(isPresented: $showingImagePicker) {
-            PhotosPicker(selection: $selectedItems,
-                                 matching: .images) {
-                        Text("Select Multiple Photos")
-                    }
-        }
-    }
-    
-    func loadImage() {
-        guard let inputImage = UIImage(named: "Example") else { return }
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
-        
-        currentFilter.inputImage = beginImage
-        
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        
-        if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
-        }
-        
-        if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-        }
-        
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        if let cgImg = context.createCGImage(outputImage, from: outputImage.extent) {
-            let uiImg = UIImage(cgImage: cgImg)
-            image = Image(uiImage: uiImg)
-        }
+        Text("Hello World")
     }
 }
 
@@ -71,3 +21,43 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+/* Demo Code
+ 
+ func loadImage() {
+     guard let inputImage = inputImage else { return }
+     image = Image(uiImage: inputImage)
+ }
+ 
+ func loadImageOld() {
+     guard let inputImage = UIImage(named: "Example") else { return }
+     let beginImage = CIImage(image: inputImage)
+     
+     let context = CIContext()
+     let currentFilter = CIFilter.sepiaTone()
+     
+     currentFilter.inputImage = beginImage
+     
+     let amount = 1.0
+     let inputKeys = currentFilter.inputKeys
+     
+     if inputKeys.contains(kCIInputIntensityKey) {
+         currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+     }
+     
+     if inputKeys.contains(kCIInputRadiusKey) {
+         currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+     }
+     
+     if inputKeys.contains(kCIInputScaleKey) {
+         currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+     }
+     
+     guard let outputImage = currentFilter.outputImage else { return }
+     
+     if let cgImg = context.createCGImage(outputImage, from: outputImage.extent) {
+         let uiImg = UIImage(cgImage: cgImg)
+         image = Image(uiImage: uiImg)
+     }
+ }
+ */
