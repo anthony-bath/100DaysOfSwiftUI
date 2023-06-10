1d056ad712ed98c32389ad882515712bcb6226b9
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-06-10 08:07:57 -0400

    Day 68 Complete

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Anthony Bath on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
