c40e137a06e8b48490b770150997d0c84609a395
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-06-16 07:13:01 -0400

    Day 75 Complete

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -31,6 +31,8 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle.fill")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel("\(word), \(word.count) letters")
                     }
                 }
             }
