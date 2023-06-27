1f0d285db3482a22840935d4368ecb7d47a4b68d
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-06-27 07:48:50 -0400

    Day 86 Complete

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Anthony Bath on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
