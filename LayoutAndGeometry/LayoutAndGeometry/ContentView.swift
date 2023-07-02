5ce6ef7e9a42fb9b2a1b2f0674ba5d500d2ade00
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-07-02 07:41:39 -0400

    Day 93 Complete

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,34 +7,85 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    struct MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
+//extension VerticalAlignment {
+//    struct MidAccountAndName: AlignmentID {
+//        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+//            context[.top]
+//        }
+//    }
+//
+//    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+//}
+//
+//struct Outerview: View {
+//    var body: some View {
+//        VStack {
+//            Text("Top")
+//            InnerView()
+//                .background(.green)
+//            Text("Bottom")
+//        }
+//    }
+//}
+//
+//struct InnerView: View {
+//    var body: some View {
+//        HStack {
+//            Text("Left")
+//            GeometryReader { geo in
+//                Text("Center")
+//                    .background(.blue)
+//                    .onTapGesture {
+//                        print("Global center: x=\(geo.frame(in: .global).midX),y=\(geo.frame(in: .global).midY)")
+//                        print("Local center: x=\(geo.frame(in: .local).midX),y=\(geo.frame(in: .local).midY)")
+//                        print("Custom center: x=\(geo.frame(in: .named("Custom")).midX),y=\(geo.frame(in: .named("Custom")).midY)")
+//                    }
+//            }
+//            .background(.orange)
+//            Text("Right")
+//        }
+//    }
+//}
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image("beach")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("Full Name:")
-                Text("Paul Hudson")
-                    .font(.largeTitle)
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    GeometryReader { geo in
+                        Text("Number \(num)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(
+                                .degrees(-geo.frame(in: .global).minX) / 5,
+                                axis: (x: 0, y: 1, z: 0)
+                            )
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
+                }
             }
         }
+//        GeometryReader { fullView in
+//            ScrollView {
+//                ForEach(0..<50) { index in
+//                    GeometryReader { geo in
+//                        Text("Row #\(index)")
+//                            .font(.title)
+//                            .frame(maxWidth: .infinity)
+//                            .background(colors[index % 7])
+//                            .rotation3DEffect(
+//                                .degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5,
+//                                axis: (x: 0, y:1, z: 0)
+//                            )
+//                    }
+//                    .frame(height: 40)
+//                }
+//            }
+//        }
     }
 }
 
