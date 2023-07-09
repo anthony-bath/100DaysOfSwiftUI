18dd9b1eaba9bbd6b9025d9a6fb0eb205e469809
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-07-09 07:59:36 -0400

    Day 99 Complete

--- SnowSneeker/SnowSneeker/ContentView.swift
+++ SnowSneeker/SnowSneeker/ContentView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum SortType {
+        case name, runs, country
+    }
+    
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
     
     @State private var resortId: String?
     @State private var searchTerm: String = ""
+    @State private var isShowingSortDialog = false
+    @State private var sortType: SortType = .name
+    @State private var columnVisibility = Binding.constant(NavigationSplitViewVisibility.detailOnly)
     @StateObject private var favorites = Favorites()
     
     var selectedResort : Resort? {
@@ -22,17 +29,26 @@ struct ContentView: View {
         return nil
     }
     
-    var filteredResorts: [Resort] {
-        if searchTerm.isEmpty {
-            return resorts
+    var filteredSortedResorts: [Resort] {
+        var filteredResorts = resorts
+        
+        if !searchTerm.isEmpty {
+            filteredResorts = resorts.filter { $0.name.localizedCaseInsensitiveContains(searchTerm) }
         }
         
-        return resorts.filter { $0.name.localizedCaseInsensitiveContains(searchTerm) }
+        switch sortType {
+        case .name:
+            return filteredResorts.sorted { $0.name < $1.name }
+        case .runs:
+            return filteredResorts.sorted { $0.runs > $1.runs }
+        case .country:
+            return filteredResorts.sorted { $0.country < $1.country }
+        }
     }
     
     var body: some View {
-        NavigationSplitView(columnVisibility: .constant(.detailOnly)) {
-            List(filteredResorts, selection: $resortId) { resort in
+        NavigationSplitView(columnVisibility: columnVisibility ) {
+            List(filteredSortedResorts, selection: $resortId) { resort in
                 NavigationLink(value: resort) {
                     HStack {
                         Image(resort.country)
@@ -61,6 +77,37 @@ struct ContentView: View {
             }
             .searchable(text: $searchTerm, prompt: "Search for a resort")
             .navigationTitle("Resorts")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        isShowingSortDialog = true
+                    } label: {
+                        Label("Sort", systemImage: "line.3.horizontal.decrease")
+                    }
+                }
+            }
+            .confirmationDialog("Sort By", isPresented: $isShowingSortDialog, titleVisibility: .visible) {
+                Button("Name") {
+                    sortType = .name
+                    isShowingSortDialog = false
+                    columnVisibility = Binding.constant(NavigationSplitViewVisibility.all)
+                }
+                
+                Button("Runs") {
+                    sortType = .runs
+                    isShowingSortDialog = false
+                    columnVisibility = Binding.constant(NavigationSplitViewVisibility.all)
+                }
+                
+                Button("Country") {
+                    sortType = .country
+                    isShowingSortDialog = false
+                    columnVisibility = Binding.constant(NavigationSplitViewVisibility.all)
+                }
+            }
+            .onChange(of: selectedResort) { _ in
+                columnVisibility = Binding.constant(NavigationSplitViewVisibility.detailOnly)
+            }
         } detail: {
             if let resort = selectedResort {
                 ResortView(resort: resort)
